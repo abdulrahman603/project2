a82c477c48e5fd00e1baab1cfd9b4b081bee5af0
abdulrahman603 <abdulrahmanshoaib5@gmail.com>
2020-07-28 23:45:51 +0200

    initial commit

---
+++ Bill Payment/app/src/main/java/com/example/electricitybillpayment/ststus.java
@@ -0,0 +1,52 @@
+package com.example.electricitybillpayment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.*;
+
+public class ststus extends AppCompatActivity {
+
+    TextView a;
+    Button pay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ststus);
+       a = (TextView)findViewById(R.id.bill);
+       String b = String.valueOf(randomPriceGenerator());
+       a.setText("Your Bill:"+ b);
+       pay = (Button) findViewById(R.id.paybutton);
+       pay.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+                Paypage();
+           }
+       });
+
+
+    }
+
+    public int randomPriceGenerator(){
+        Random rn = new Random();
+        int n = 10000 - 1000 + 1;
+        int i = rn.nextInt() % n;
+        if (i<0){
+            return (-i);
+        }
+        else {
+        return(1000 + i);}
+    }
+
+    public void Paypage(){
+        Intent intent = new Intent(this,pay.class);
+        startActivity(intent);
+    }
+
+
+}
